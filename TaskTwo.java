67064ae7fee17580d7819647111709028260b8fe
natalie-cafune <nataly.pavelko@gmail.com>
2018-11-02 17:53:06 +0200

    Улучшила ввод и читабельность

--- TaskTwo.java
+++ TaskTwo.java
@@ -6,28 +6,30 @@ public class TaskTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your name : ");
+        System.out.print("Enter your name : ");
         String name = sc.nextLine();
 
-        System.out.println("Enter your city : ");
+        System.out.print("Enter your city : ");
         String city = sc.nextLine();
 
-        System.out.println("Enter your age : ");
+        System.out.print("Enter your age : ");
         int age = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Enter your hobby : ");
+        System.out.print("Enter your hobby : ");
         String hobby = sc.nextLine();
 
+        System.out.println("");
         System.out.println("Вывод первого варианта");
-
-        System.out.println("Вывод второго варианта");
         System.out.print("Name:  " + name + "\n" + "City:  " +  city + "\n"
                 + "Age:   " + age + "\n" + "Hobby: " + hobby + "\n" );
 
+        System.out.println("");
+        System.out.println("Вывод второго варианта");
         System.out.println("Человек по имени " + name + " живет в городе " + city +
                 ". Этому человеку " + age + " лет и любит заниматься " + hobby);
 
+        System.out.println("");
         System.out.println("Вывод третьего варианта");
         System.out.print( name + " - name"  + "\n" + city + " - city" + "\n"
                 + age + " - age" + "\n" + hobby + " - hobby" + "\n" );
